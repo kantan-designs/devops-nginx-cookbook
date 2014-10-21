f51fee885ec052082347b7a08616ee9a665c53e3
John McDowall <john@mcdowall.info>
2014-10-21 12:29:37 -0700

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name              "devops-nginx-cookbook"
+maintainer        "John McDowall"
+maintainer_email  "john@kantan.io"
+description       "Installs the nginx web server from the nginx ppa"
+version           "0.0.1"
+
+recipe "devops-nginx-cookbook", "nginx server"
+
+supports "ubuntu"
